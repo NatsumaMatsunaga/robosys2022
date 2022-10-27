9edd7ea041ceb9e8ff53119dd12bfcaa69e08041
Natsuma Matsunaga <natsuma0722@gmail.com>
2022-10-27 12:51:11 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Natsuma Matsunaga
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -12,6 +12,17 @@ res=0
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+
+### STRANGE INPUT ###
+out=$(echo | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 [ "${res}" = 0 ] && echo OK
 exit $res
