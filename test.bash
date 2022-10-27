119633fb53c94573ea9d3b10432a23d896a53cca
Natsuma Matsunaga <natsuma0722@gmail.com>
2022-10-27 12:31:10 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -1,3 +1,17 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2022 Natsuma Matsunaga
+# SPDX-License-Identifier: BSD-3-Clause
 
-seq 5 | ./plus
+ng () {
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
+
+### I/O TEST ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng ${LINENO}
+
+[ "${res}" = 0 ] && echo OK
+exit $res
